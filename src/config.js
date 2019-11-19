57ba3968f53672b15cdd6e628b917bcfd5e3df65
NandosJake <39186454+NandosJake@users.noreply.github.com>
2019-11-19 10:37:30 +0000

    .

--- src/config.js
+++ src/config.js
@@ -1,10 +1,5 @@
-// eslint-disable-next-line no-unused-vars
-const dotenv = require('dotenv').config();
-
-const { INFRASTRUCTURE_WEBSERVER_PORT } = process.env;
-
 module.exports = {
   webserver: {
-    port: parseInt(INFRASTRUCTURE_WEBSERVER_PORT, 10),
+    port: 5000,
   },
 };
