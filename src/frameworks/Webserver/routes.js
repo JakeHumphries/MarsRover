f1bf1f7126b5eb646fa99630eb6d75480978fe03
NandosJake <39186454+NandosJake@users.noreply.github.com>
2019-11-20 12:08:17 +0000

    .

--- src/frameworks/Webserver/routes.js
+++ src/frameworks/Webserver/routes.js
@@ -16,7 +16,7 @@ router.get('/createRover', (req, res) => {
 
 router.get('/clearRovers', (req, res) => {
   clearRovers();
-  res.json('Succesfully cleared rovers from grid - YOU ARE IN TEST');
+  res.json('Succesfully cleared rovers from grid - YOU ARE IN TESTING');
 });
 
 router.get('/moveRover', (req, res) => {
