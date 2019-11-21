2a32404f96381e797994d1e26f12db91300c23ad
NandosJake <39186454+NandosJake@users.noreply.github.com>
2019-11-21 15:53:02 +0000

    .

--- src/frameworks/Webserver/routes.js
+++ src/frameworks/Webserver/routes.js
@@ -11,7 +11,7 @@ router.get('/createRover', (req, res) => {
   const { startingY } = req.query;
   const { startingDir } = req.query;
   dropRover(roverID, startingX, startingY, startingDir);
-  res.json(`Succesfully dropped rover ${roverID} at position ${startingX},${startingY},${startingDir} this is a massive change 1`);
+  res.json(`Succesfully dropped rover ${roverID} at position ${startingX},${startingY},${startingDir} this is a massive change 1000`);
 });
 
 router.get('/clearRovers', (req, res) => {
